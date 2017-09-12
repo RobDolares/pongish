abd27db2cae9541c89453ceb08620468bb33b547
Robert Howington <zerorob2787@gmail.com>
2017-09-12 14:45:26 -0400

    finish basic functionality with scoring - need to reset score upon cap and style

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ let ballY = 50;
 let ballVeloX = 10;       //ball x velocity
 let ballVeloY = 6;        //ball y velocity
 let pad1Y = 250;
-const padHeight = 100;
-let padWidth = 10;
+let pad2Y = 250;
+const padHeight = 100;    //paddle dimensions
+const padWidth = 10;
+let player1score = 0;
+let player2score = 0;
+
 
 
 window.onload=()=>{
@@ -19,13 +23,14 @@ window.onload=()=>{
   },1000/fps);
   canvas.addEventListener('mousemove',(e)=>{
     let mousePos = calcMousePos(e);
-    pad1Y = mousePos.y-(padHeight/2);
+    pad1Y = mousePos.y - (padHeight/2);
   })
 }
 
 // ***** reset gameball ***** //
 
 function reset(){
+
   ballVeloX = -ballVeloX;
   ballX = canvas.width/2;
   ballY = canvas.height/2;
@@ -54,8 +59,14 @@ function drawEverything(){
   //left player paddle
   colorRect(0,pad1Y,padWidth,padHeight, 'white');
 
+  //artificial int paddle
+  colorRect(canvas.width - padWidth, pad2Y, padWidth, padHeight, 'white');
+
   //draw the ball
   colorCircle(ballX, ballY, 5,'white');
+
+  canvasContext.fillText(player1score, 100, 100);
+  canvasContext.fillText(player2score, canvas.width-100, canvas.height-380);
 
 }
 
@@ -73,24 +84,46 @@ function colorRect(leftX, topY, width, height, drawColor){
 
 //*****  movement and collisions ***** //
 
+// ***** AI ***** //
+
+function aiMovement(){
+  let pad2YCenter = pad2Y + (padHeight/2);
+  if (pad2YCenter < ballY-35) {
+    pad2Y += 6;
+  } else if ( pad2YCenter > ballY+35){
+    pad2Y -= 6;
+  }
+}
+
 function moveEverything(){
+  aiMovement();
 
   ballX += ballVeloX;
   ballY += ballVeloY;
-
 
   //ball bouncing off left/right
   if (ballX < 0) {
     if (ballY > pad1Y && ballY < pad1Y + padHeight) {
       ballVeloX = -ballVeloX;
+      let deltaY = ballY - (pad1Y+padHeight/2)
+      ballVeloY = deltaY * 0.35;
     } else {
+      player2score++;
       reset();
     }
+  }
 
-  }
   if (ballX > canvas.width) {
-    ballVeloX = -ballVeloX;
+    if (ballY > pad2Y && ballY < pad2Y + padHeight) {
+      ballVeloX = -ballVeloX;
+      let deltaY = ballY - (pad2Y+padHeight/2)
+      ballVeloY = deltaY * 0.35;
+    } else {
+      player1score++;
+      reset();
+    }
   }
+
   //ball bouncing off top/bottom
   if (ballY < 0) {
 
