a06e3cd310e4f9a04b9667fe7b2c58e8cec3bd03
Robert Howington <zerorob2787@gmail.com>
2017-09-12 12:10:49 -0400

    create left paddle and ball

--- index.js
+++ index.js
@@ -13,9 +13,20 @@ function drawEverything(){
   // black bgc
   colorRect(0,0,canvas.width,canvas.height, 'black');
 
+  //left player paddle
+  colorRect(0,210,10,100, 'white');
+
+  //draw the ball
+  colorCircle(ballX, ballY, 5,'white');
 
 }
 
+function colorCircle (centerX, centerY, radius, drawColor){
+  canvasContext.fillStyle = drawColor;
+  canvasContext.beginPath();
+  canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true );
+  canvasContext.fill();
+}
 
 function colorRect(leftX, topY, width, height, drawColor){
   canvasContext.fillStyle = drawColor;
