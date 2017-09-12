63a854f310343c8cf12548e8ffbff50503e3501e
Robert Howington <zerorob2787@gmail.com>
2017-09-12 13:27:34 -0400

    trigger reset when ball passes left paddle

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ let canvasContext;
 let ballX = 50;
 let ballY = 50;
 let ballVeloX = 10;       //ball x velocity
-let ballVeloY = 4;        //ball y velocity
+let ballVeloY = 6;        //ball y velocity
+let pad1Y = 250;
+const padHeight = 100;
+let padWidth = 10;
 
 
 window.onload=()=>{
@@ -14,7 +17,22 @@ window.onload=()=>{
     moveEverything();
     drawEverything();
   },1000/fps);
+  canvas.addEventListener('mousemove',(e)=>{
+    let mousePos = calcMousePos(e);
+    pad1Y = mousePos.y-(padHeight/2);
+  })
 }
+
+// ***** reset gameball ***** //
+
+function reset(){
+  ballVeloX = -ballVeloX;
+  ballX = canvas.width/2;
+  ballY = canvas.height/2;
+
+}
+
+// ***** mouse movement ***** //
 
 function calcMousePos(e){
   let rect = canvas.getBoundingClientRect();
@@ -34,7 +52,7 @@ function drawEverything(){
   colorRect(0,0,canvas.width,canvas.height, 'black');
 
   //left player paddle
-  colorRect(0,210,10,100, 'white');
+  colorRect(0,pad1Y,padWidth,padHeight, 'white');
 
   //draw the ball
   colorCircle(ballX, ballY, 5,'white');
@@ -63,13 +81,19 @@ function moveEverything(){
 
   //ball bouncing off left/right
   if (ballX < 0) {
-    ballVeloX = -ballVeloX;
+    if (ballY > pad1Y && ballY < pad1Y + padHeight) {
+      ballVeloX = -ballVeloX;
+    } else {
+      reset();
+    }
+
   }
   if (ballX > canvas.width) {
     ballVeloX = -ballVeloX;
   }
   //ball bouncing off top/bottom
   if (ballY < 0) {
+
     ballVeloY = -ballVeloY;
   }
   if (ballY > canvas.height) {
